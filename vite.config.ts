da90496e6ed1d9fe04c37bfbf398dc19c4839967
Shubham Chaudhari <151992419+Winter262005@users.noreply.github.com>
2026-07-16 20:08:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hmu-website/', // Add this line (replace with your exact repo name if different)
   plugins: [react(), tailwindcss()],
 })
