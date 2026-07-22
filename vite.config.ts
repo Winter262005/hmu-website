51356c4e1a0df9a858da13ca6bbacf6df7f565d0
Winter262005 <shubhampc262005@gmail.com>
2026-07-22 23:40:37 +0530

    vite config for CUSTOM DOMAIN

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hmu-website/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
